20fc9ff8bc1ffcc7b10cf3b82bcbf0e77f45db5a
baru-d <barudaniel6@gmail.com>
2023-09-26 07:35:37 +0300

    funtions continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    let ans = `This Saturday, I want to ${activity}!`;
+    return ans;
+  }
+  
+  function mondayWork(activity = "go to the office") {
+    let ans = `This Monday, I will ${activity}.`;
+    return ans;
+  }
+  
+  function wrapAdjective(symbol = "*") {
+    const inner = function (theAdjective = "special") {
+      return `You are ${symbol}${theAdjective}${symbol}!`;
+    };
+    return inner;
+  }
